b080f7a15a4ba9a9ec665ef360ebb35fbd2129db
Griffin <chemist_khnu@rambler.ru>
2020-09-07 18:22:52 +0300

    Step 1 added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import { v4 as uuidv4 } from 'uuid'; 
+
 class App extends Component {
 
   state = {
@@ -10,6 +12,18 @@ class App extends Component {
 
   handleChange = (evt) => {
     this.setState({name: evt.target.value});
+  };
+
+  handleSubmit = (evt) => {
+    evt.preventDefault();
+    console.log(this.state.contacts);
+    if (this.state.name === "") {return};
+    const contact = {
+      id: uuidv4(),
+      name: this.state.name
+    };
+    this.setState({contacts: [...this.state.contacts, contact]});
+    this.setState({name: ''});
   }
 
   render() {
@@ -17,14 +31,14 @@ class App extends Component {
   return (
     <div>
       <h1>Phonebook</h1>
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <label>Name</label>
         <input type="text" value={name} placeholder="Input name..." onChange={this.handleChange}></input>
         <button type="submit">Add contact</button>
       </form>
       <h2>Contacts</h2>
       <ul className="contactList">
-
+       {this.state.contacts.map(({ id, name }) => (<li key={id}>{name}</li>))}
       </ul>
     </div>
   )}
