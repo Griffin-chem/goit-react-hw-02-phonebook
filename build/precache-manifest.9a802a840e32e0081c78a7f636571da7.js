c03c89f38f969246779977848135555137aaa21e
Griffin <chemist_khnu@rambler.ru>
2020-09-14 01:14:41 +0300

    Deploy

---
+++ build/precache-manifest.9a802a840e32e0081c78a7f636571da7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e9db1558773e4dc1c39860a0eb513b7",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "81a64818097c87dd148e",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "92f11fbbdbf16a4d43ad",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.41ec1cbc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.41ec1cbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81a64818097c87dd148e",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.fbc3b44a.chunk.js"
+  },
+  {
+    "revision": "cd9cb9861ff147993a85",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.0dd797ef.js"
+  }
+]);
